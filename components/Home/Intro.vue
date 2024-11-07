82e3ba673bd498ac5e5be4fa35f5d00ea91973a6
Tejus Sahi <tejussahi07@gmail.com>
2024-11-07 23:55:46 +0530

    Update author details and add deploy script
    This commit updates mentions of "Fayaz Ahmed" to "Tejus Sahi" in the Footer component and article author metadata. Additionally, a deploy script and related dependencies have been added to the `package.json` file. The `yarn.lock` file is updated to reflect the new package installations.

--- components/Home/Intro.vue
+++ components/Home/Intro.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="space-y-6">
-    <NuxtImg src="/avatar.png" alt="Fayaz Ahmed"
+    <NuxtImg src="/avatar.png" alt="Tejus Sahi"
       class="ring-2 border ring-gray-200 border-gray-300 dark:ring-white/10 dark:border-gray-800 hover:ring-4 transition-all duration-300 bg-gray-200 dark:bg-gray-900 rounded-full h-12 w-12 sm:h-16 sm:w-16"
       sizes="48px sm:64px" placeholder format="webp" />
     <h1 class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100">
       Hello!
     </h1>
     <p class="text-gray-900 dark:text-gray-400">
-      I'm Fayaz, I work as a software, product engineer and designer from
-      Bengaluru, India. I specialize in building web applications and sites
+      I'm Tejus, I work as a software engineer from Delhi, India.
+      I specialize in building web applications and sites
       using Javascript, React, Vue &amp; Node. I've procrastinated building this
       website for years but finally it's here, I've carved out my own little
       nook on the internet to share my silly experiments, nifty projects, and
@@ -25,8 +25,8 @@
 
 <script setup>
 useSeoMeta({
-  title: "Fayaz Ahmed",
+  title: "Tejus Sahi",
   description:
-    "I'm Fayaz, your friendly neighborhood software, product engineer and designer from Bengaluru, India. I specialize in building web applications and sites using Javascript, React, Vue & Node.",
+    "I'm Tejus, your friendly neighborhood software, product engineer and designer from Bengaluru, India. I specialize in building web applications and sites using Javascript, React, Vue & Node.",
 });
 </script>
